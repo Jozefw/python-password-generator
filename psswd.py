db592eee716f814fef856a5a3e0fe49eb96bdc07
Jozef <Jozefw@sbcglobal.net>
2025-02-14 15:35:21 -0800

    easy randomizer

--- psswd.py
+++ psswd.py
@@ -8,3 +8,20 @@ print("Welcome to the Python Password Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+psswd = ''
+for letter in range(1,nr_letters+1):
+    rando_char = random.choice(letters)
+    psswd += rando_char
+    print(psswd)
+
+for number in range(1,nr_numbers+1):
+    rando_num = random.choice(numbers)
+    psswd += rando_num
+    print(psswd)
+
+for sym in range(1,nr_symbols+1):
+    rando_sym = random.choice(symbols)
+    psswd += rando_sym
+    print(psswd)
+#print(psswd)  
